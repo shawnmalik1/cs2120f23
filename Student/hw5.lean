4be266b15e0e1d698dbefc5d4fabc4fb4a7aa6e3
Shawn <wbr4az@virginia.edu>
2023-09-25 23:47:30 -0400

    s

--- Student/hw5.lean
+++ Student/hw5.lean
@@ -1,5 +1,5 @@
 /-!
-# Homework 5: Inhabitedness
+# Homework 5: Inhabitedness and Induction
 
 The PURPOSE of this homework is to greatly strengthen 
 your understanding of reasoning with sum and product
@@ -19,7 +19,7 @@ uses.
 
 /-!
 
-## PART 1
+## PART 1: Inhabitedness and Logical Negation
 
 Of particular importance in these questions is the
 idea that *having* a function value (implementation) 
@@ -56,11 +56,28 @@ that there is no cheese, and shows *in either case*
 that there can be no jam AND cheese. 
 -/
 
+/-!
+## New Addition: no (α : Type) := α → Empty
+
+We can make the logical intent of our
+types and computations clearer by introducing a
+shorthand, *no α* for the type *α → Empty*. Then
+in each place where a type like *α → Empty appears
+in this homework, replace it with *no α*. Use the
+right local names in each instance, of course. 
+-/
+def no (α : Type) := α → Empty
+
+/-!
+We've now replaced each α → Empty with no α. We
+suggest that you go ahead and use *no* wherever
+doing so makes the logical meaning clearer. 
+-/
 def not_either_not_both { jam cheese } :
-  ((jam → Empty) ⊕ (cheese → Empty)) → 
-  (jam × cheese → Empty) 
-| Sum.inl nojam => (fun nocheese => Empty)
-| Sum.inr nocheese => (fun nojam => Empty)
+  ((no jam) ⊕ (no cheese)) → 
+  (no (jam × cheese)) 
+| Sum.inl nojam => (fun (jam, _) => nojam jam)
+| Sum.inr nocheese => (fun(_, cheese) => nocheese cheese)
 
 /-!
 ### #2: Not One or Not the Other Implies Not Both
@@ -74,8 +91,8 @@ names, *jam* and *cheese*.
 -/
 
 def demorgan1  {α β : Type} : ((α → Empty) ⊕ (β → Empty)) → (α × β → Empty)  
-| (Sum.inl noa) => _
-| (Sum.inr nob) => _
+| (Sum.inl noa) => fun(a , b) => noa a 
+| (Sum.inr nob) => fun(a, b) => nob b
 
 /-!
 ### #3: Not Either Implies Not One And Not The Other
@@ -89,7 +106,7 @@ given *any* types, α and β,
 -/
 
 def demorgan2 {α β : Type} : (α ⊕ β → Empty) → ((α → Empty) × (β → Empty))
-| noaorb => _
+| noaorb => (λ a : α, noaorb (Sum.inl a), λ b : β, noaorb (Sum.inr b))
 
 
 /-!
@@ -102,12 +119,170 @@ Hint: You might want to use an explicit match expression
 in writing your solution.
 -/
 def demorgan3 {α β : Type} : ((α → Empty) × (β → Empty)) → ((α ⊕ β) → Empty)  
-| _ => _
+| (noa, nob) => 
+  λ ab : α ⊕ β =>
+    match ab with
+    | Sum.inl a => noa a
+    | Sum.inr b => nob b
 
 /-!
 ## PART 2
 
-Coming Soon.
+The following problems aim to strengthen your 
+understanding of inductive type definitions and
+recusrive functions.
 -/
+
+-- Here are some named Nat values, for testing
+def n0 := Nat.zero
+def n1 := Nat.succ n0
+def n2 := Nat.succ n1
+def n3 := Nat.succ n2
+def n4 := Nat.succ n3
+def n5 := Nat.succ n4
+
+/-!
+### #1. Pattern Matching Enables Destructuring
+
+#1: Defne a function, pred: Nat → Nat, that takes an any
+Nat, n, and, if n is zero, returns zero, otherwise analyze
+n as (Nat.succ n') and return n'. Yes this question should
+be easy. Be sure you understand destructuring and pattern
+matching.  
+-/
+
+-- Here
+def pred : Nat → Nat 
+| 0 => 0
+| (Nat.succ n') => n'
+
+
+
+-- Test cases
+#reduce pred 3    -- expect 2
+#reduce pred 0    -- expect 0
+
+/-!
+### #2. Big Doll from Smaller One n Times
+
+Write a function, *mk_doll : Nat → Doll*, that takes
+any natural number argument, *n*, and that returns a doll 
+n shells deep. The verify using #reduce that (mk_doll 3)
+returns the same doll as *d3*. 
+-/
+
+-- Answer here
+inductive Doll
+| empty : Doll
+| nested : Doll → Doll
+
+def mk_doll : Nat → Doll
+| 0 => Doll.empty
+| (Nat.succ n) => Doll.nested (mk_doll n)
+
+-- test cases
+#check mk_doll 3
+#reduce mk_doll 3
+
+/-!
+### #3. A Boolean Nat Equality Predicate
+
+Write a function, *nat_eq : Nat → Nat → Bool*, that
+takes any two natural numbers and that returns Boolean 
+*true* if they're equal, and false otherwise. Finish
+off the definition by filling the remaining hole (_).
+-/
+
+def nat_eq : Nat → Nat → Bool
+| 0, 0 => true
+| 0, n' + 1 => false
+| n' + 1, 0 => false
+| (n' + 1), (m' + 1) => nat_eq n' m'
+
+-- a few tests
+#eval nat_eq 0 0
+#eval nat_eq 0 1
+#eval nat_eq 1 0
+#eval nat_eq 1 1
+#eval nat_eq 2 0
+#eval nat_eq 2 1
+#eval nat_eq 2 2
+
+
+/-!
+### #4. Natural Number Less Than Or Equal
+
+Write a function, *nat_le : Nat → Nat → Bool*, that
+takes any two natural numbers and that returns Boolean 
+*true* if the first value is less than or equal to the 
+second, and false otherwise. Hint: what are the relevant 
+cases? Match to destructure them then return the right
+result *in each case*.
+-/
+
+-- Here
+def nat_le: Nat → Nat → Bool
+| 0, _ => true
+| _, 0 => false
+| (n' + 1), (m' + 1) => nat_le n' m'
+
+
+/-!
+###  #5. Nat Number Addition 
+
+Complete this function definition to implement
+a natural number addition function. 
+ -/
+
+def add : Nat → Nat → Nat
+| m, 0 => m
+| m, (Nat.succ n') => Nat.succ (add m n')   -- hint: recursion
+
+
+-- Some test cases
+#reduce add 0 0   -- expect 0
+#reduce add 5 0   -- expect 5
+#reduce add 0 5   -- expect 5
+#reduce add 5 4   -- expect 9
+#reduce add 4 5   -- expect 9
+#reduce add 5 5   -- expect 10
+
+
+/-!
+###  #6. Natural Number Multiplication 
+
+Complete this function definition to implement
+a natural number multiplication function. You
+can't use Lean's Nat multiplication function.
+Your implementation should use productively
+the add function you just definied. Wite a few
+test cases to show that it appears to be working. 
+ -/
+
+def mul : Nat → Nat → Nat
+| m, 0 => 0
+| m, (Nat.succ n') => add (m) (mul m n')
+
+#eval mul 5 3 --15
+#eval mul 2 8 --16
+
+/-!
+### Sum Binary Nat Function Over Range 0 to n 
+Define a function, sum_f, that takes a function,
+f : Nat → Nat and a natural number n, and that
+returns the sum of all of the values of (f k)
+for k ranging from 0 to n. 
+
+Compute expected results by hand for a few
+test cases and write the tests using #reduce. 
+For example, you might use the squaring function
+as an argument, with a nat, n, to obtain the 
+sum of the squares of all the numbers from 0
+to and including n. 
+-/
+
+def sum_f : (Nat → Nat) → Nat → Nat 
+| f, 0 => f 0
+| f, n' + 1 => f (n' + 1) + sum_f f n'
 
 
